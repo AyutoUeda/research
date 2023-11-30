275b42d61018f71f3b30fe4cf2e8081be1c938ee
AyutoUeda <ayuto.ueda@gmail.com>
2023-12-01 00:06:25 +0900

    add 10droplets/data_processing.py which can create data

---
+++ 10droplets/data_processing.py
@@ -0,0 +1,101 @@
+from typing import Any
+import pandas as pd
+import numpy as np
+import math
+
+class DataProcessing:
+    """入力されたデータを基準からの距離と角度に変換するクラス
+    input_data: 入力データ(numpy.array)
+    n_nearest_neighbors: 近傍点の数(int)
+    
+    return:
+        labels: 各時刻におけるtargetのラベル(numpy.array), 30度ごとにラベルを振る
+            [基準からの角度1, 基準からの角度2, ...]
+        data_d_and_angle: 各時刻におけるtargetとの距離と角度(numpy.array) 
+            [基準からの距離1, 基準からの角度1, 基準からの距離2, 基準からの角度2, ...]
+    """
+    def __init__(self, input_data, n_nearest_neighbors): 
+        self.input_data = input_data[1:] # ベクトルの計算のために1つずらす
+        self.vecotrs = np.diff(input_data, axis=0)
+        self.n_nearest_neighbors = n_nearest_neighbors
+        
+    def __call__(self):
+        self.labels, self.data_d_and_angle = self.data_create()
+        
+    def data_create(self):
+        """
+        入力データを基準からの距離と角度に変換する関数
+        また、基準から最も近い点をn_nearest_neighbors個取得する
+        """
+        
+        labels = []
+        data_d_and_angle = []
+
+        for i in range(len(self.input_data) - 1):
+            temp_coordinate = self.input_data[i] # i秒目における各点の座標
+            x_target = temp_coordinate[0] # # i秒目におけるtargetのx座標
+            y_target = temp_coordinate[1] # # i秒目におけるtargetのy座標
+
+            vectors = self.vectors[i] # <-- 次の時刻との差
+
+            target_vector = np.arctan(vectors[1] / vectors[0]) * 180 / np.pi
+
+            # 360度に変換
+            if target_vector < 0:
+                target_vector = x - 360 * math.floor(target_vector/360)
+
+            label = target_vector // 30
+
+            labels.append(label)
+
+            temp_list = []
+            # 各点とtargetを比較
+            for j in range(2,temp_coordinate.shape[1], 2):
+                # 基準となる座標と比べる座標の差
+                x_diff = x_target - temp_coordinate[j]
+                y_diff = y_target - temp_coordinate[j+1]
+
+                tan = y_diff / x_diff
+                
+                # 基準との距離
+                d = math.sqrt(x_diff ** 2 + y_diff ** 2)
+                temp_list.append(d)
+
+                # 基準から見た角度
+                atan = np.arctan(tan) * 180 / np.pi
+                temp_list.append(atan)
+                
+            
+            # 基準から近い点をn_nearest個取得
+            temp_list = np.array(temp_list)
+            min_indices = np.argsort(temp_list[::2])[:self.n_nearest_neighbors]
+            
+            temp_data = []
+            for j in min_indices:
+                temp_data.append(temp_list[2*j]) # 基準からの距離
+                temp_data.append(temp_list[2*j+1]) # 基準からの角度
+
+            data_d_and_angle.append(temp_data)
+        
+        return np.array(labels), np.array(data_d_and_angle)
+        
+    def __len__(self):
+        return len(self.data_d_and_angle)
+    
+if __name__ == "__main__":
+    print("DataProcessing")
+    
+    def mk_dataframe(path):
+        df = pd.read_table(path, sep=" ", header=None)
+        df = df.drop(columns=0, axis=1)
+        df = df.drop(columns=df.shape[1], axis=1)
+        return df
+
+    data_path = "~/Documents/Research/neuralnetwork/position_data/10droplets/231030/pos-0.dat"
+    df = mk_dataframe(data_path)
+    
+    input_data = df.values
+    
+    data_processing = DataProcessing(input_data, n_nearest_neighbors=3)
+    
+    length = len(data_processing)
